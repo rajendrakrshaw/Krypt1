3450343ebca1e4dce191ecad192b0c6651809a17
Nikhil-550 <nikhilshaw7092003@gmail.com>
2025-07-16 23:46:06 +0530

    Update vite.config.js for Vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
-
-
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
+});
+
 
 
 
