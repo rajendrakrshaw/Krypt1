e3d21fc5f7ca17f40dde4d9fb7b19c7e4526a7ef
rajendrakrshaw <rajendra250601@gmail.com>
2025-07-26 11:31:34 +0530

    fix error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,6 +13,6 @@ createRoot(document.getElementById('root')).render(
   <TransactionProvider>
     <StrictMode>
       <App />
-    </StrictMode>,
+    </StrictMode>
   </TransactionProvider>
 )
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
 // vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
+     
   },
+  plugins: [react()],
 });
 
 
